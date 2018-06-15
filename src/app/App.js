63e7c0d471ae8f32cdd5bbbf1f30a2ccb3452442
Edward Llanca Haro <ellanca@belatrixsf.com>
2018-06-14 22:02:01 -0500

    arch allMotorcycles

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor() {
+    super();
+        this.state = {
+        }
+    }
+
+
+  render() {
+    return (
+        <h1>Hola mundo</h1>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: './src/app/index.js',
+    output: {
+        path: __dirname + '/src/public/js',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['env']
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
