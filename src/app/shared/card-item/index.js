755273608d75f48a117b0d42f65260830109d058
Edward David Llanca Haro <ellanca@Macmini-de-ELLANCA.local>
2019-03-05 10:42:21 -0500

    add pictures all products

--- src/app/shared/card-item/index.js
+++ src/app/shared/card-item/index.js
@@ -6,7 +6,7 @@ const CardItem = ({ product = {}, handleProduct = () => {} }) => {
         <div className="col-lg-4 col-md-4 mb-lg-0 mb-4 item-sell">
             <div className="card collection-card z-depth-1-half">
                 <div className="view zoom">
-                <img src={(product.pictures[0].url ? '../images/ACS00001/ACS00001_1.png' : 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/8.jpg')} className="img-fluid" alt="" />
+                <img src={(product.pictures[0].url ? product.pictures[0].url : '../images/ACS00001/ACS00001_1.png')} className="img-fluid" alt="" />
                 <div className="stripe light" onClick={handleProduct.bind(product)}>
                     <a>
                     <p>{product.name}</p>
